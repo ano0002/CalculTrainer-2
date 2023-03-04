1b4cb2a167a415cfae40ef3c748d69dbf5874af7
Anatole Sot <47571181+ano0002@users.noreply.github.com>
2023-03-04 16:12:58 +0100

    Worked on a style loader

--- src/modules/load_theme.js
+++ src/modules/load_theme.js
@@ -1,20 +1,29 @@
 import Cookies from "js-cookie";
 
-function safe_set(name, value) {
+function safeSet(name, value) {
     if (Cookies.get("accepted_policy")) {
         Cookies.set(name, value, { expires: 365 });
     }
 }
 
-const loadTheme = () => {
-    const theme = Cookies.get("theme");
-    if (theme) {
-        document.documentElement.style.setProperty("--primary-color", theme);
+function loadProperty(name, default_value) {
+    const value = Cookies.get(name);
+    if (value) {
+        document.documentElement.style.setProperty(`--${name}`, value);
     }
     else {
-        document.documentElement.style.setProperty("--primary-color", "#000000");
-        safe_set("theme", "#000000");
+        safeSet(name, default_value);
+        document.documentElement.style.setProperty(`--${name}`,default_value);
     }
+}
+
+
+const loadTheme = () => {
+    loadProperty("primary_color", "#7D4E57");
+    loadProperty("secondary_color", "#E7F9A9");
+    loadProperty("cacti_color", "#09814A");
+    loadProperty("ground_black", "#1A1F16");
+    loadProperty("kangaroo_orange", "#FF8600");
 }
 
 export default loadTheme;
